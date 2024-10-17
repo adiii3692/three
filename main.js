9f3dcb8cfee30e0e31493c2915061708641c3387
adiii3692 <adinair0206@gmail.com>
2024-10-17 14:04:25 -0500

    Got the animations to work

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import {RoomEnvironment} from 'three/addons/environments/RoomEnvironment.js'
+import Stats from 'three/addons/libs/stats.module.js';
 
 const canvas = document.getElementById('canvas');
 
@@ -11,18 +12,15 @@ scene.background = new THREE.Color( 0xbfe3dd );
 
 //Add the camera
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
-camera.position.z = 15;
-camera.position.x = 15;
+camera.position.z = 10;
+camera.position.y = 10;
 
 //Add lighting
 const light = new THREE.DirectionalLight('#FFFFFF',1);
 scene.add( light);
 
-//Load the other model
-const loader =  new GLTFLoader().setPath('/')
-loader.load('iceberg.glb',(glb)=>{
-  scene.add(glb.scene);
-})
+//Mixer for animation loop
+let mixer;
 
 //Set up the renderer
 const renderer = new THREE.WebGLRenderer({canvas,alpha:true, antialias:true});
@@ -32,20 +30,41 @@ document.body.appendChild( renderer.domElement );
 const pmremGenerator = new THREE.PMREMGenerator( renderer );
 scene.environment = pmremGenerator.fromScene( new RoomEnvironment(),0.01).texture;
 
+//Load the other model
+const loader =  new GLTFLoader().setPath('/')
+loader.load('iceberg.glb',(glb)=>{
+  const model = glb.scene;
+  scene.add(model);
+
+  mixer = new THREE.AnimationMixer(model);
+  mixer.clipAction( glb.animations[ 0 ] ).play();
+  renderer.setAnimationLoop( animate );
+})
+
 //Add OrbitControls
 const controls = new OrbitControls(camera,renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.05;
-controls.enableZoom = true;
+controls.enableZoom = false;
 controls.enablePan = true;
 
+//Clock for animations
+const clock = new THREE.Clock();
 
+const stats = new Stats();
+canvas.appendChild( stats.dom );
 
 //Animate the scene
 function animate(){
-    requestAnimationFrame(animate);
+  const delta = clock.getDelta();
 
-    renderer.render(scene,camera);
+  mixer.update( delta );
+
+  controls.update();
+
+  stats.update();
+
+  renderer.render( scene, camera );
 } 
 
 //Handle window resizing
