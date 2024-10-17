59a929193b5260aee6da38544c350f2389204d30
adiii3692 <adinair0206@gmail.com>
2024-10-17 13:41:12 -0500

    Added iceberg and got the textures right somehow

--- main.js
+++ main.js
@@ -1,33 +1,36 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import {RoomEnvironment} from 'three/addons/environments/RoomEnvironment.js'
 
 const canvas = document.getElementById('canvas');
 
 //create the scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color('#F0F0F0');
+scene.background = new THREE.Color( 0xbfe3dd );
 
 //Add the camera
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
-camera.position.z = 5;
-
-//Create the cube
-const geometry = new THREE.DodecahedronGeometry();
-const material =   new THREE.MeshLambertMaterial({color:'#468585',emissive:'#468585'})
-
-const cube = new THREE.Mesh(geometry,material);
-scene.add(cube);
+camera.position.z = 15;
+camera.position.x = 15;
 
 //Add lighting
-const light = new THREE.SpotLight(0x006769, 100);
-light.position.set(1,1,1,);
-scene.add(light);
+const light = new THREE.DirectionalLight('#FFFFFF',1);
+scene.add( light);
+
+//Load the other model
+const loader =  new GLTFLoader().setPath('/')
+loader.load('iceberg.glb',(glb)=>{
+  scene.add(glb.scene);
+})
 
 //Set up the renderer
-const renderer = new THREE.WebGLRenderer({canvas});
+const renderer = new THREE.WebGLRenderer({canvas,alpha:true, antialias:true});
 renderer.setSize(window.innerWidth,window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild( renderer.domElement );
+const pmremGenerator = new THREE.PMREMGenerator( renderer );
+scene.environment = pmremGenerator.fromScene( new RoomEnvironment(),0.01).texture;
 
 //Add OrbitControls
 const controls = new OrbitControls(camera,renderer.domElement);
@@ -36,20 +39,12 @@ controls.dampingFactor = 0.05;
 controls.enableZoom = true;
 controls.enablePan = true;
 
-//Load the other model
-const loader =  new GLTFLoader().setPath('/pessima-broken/')
-loader.load('pessima.glb',(glb)=>{
-  const mesh = glb.scene;
-  mesh.position.set(0,1.05,-1);
-  scene.add(mesh);
-})
+
 
 //Animate the scene
 function animate(){
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
     renderer.render(scene,camera);
 } 
 
